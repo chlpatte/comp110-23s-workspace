2edc336ce023b8acf7180239de275f5645b04bc1
Christarian Woods Patterson <chlpatte@unc.edu>
2023-01-26 23:15:48 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,56 +7,166 @@ character: str = input("Enter a single character: ").lower()
 if (len(character) != 1):
     exit("Error: Character must be a single character.")
 print("Searching for " + character + " in " + word)
-index = word.find(character)
-index2 = word.rfind(character)
-if (index == 0):
-    if (index2 == 1):
-        print(character + " found at index 0")
-        print(character + " found at index 1")
-    elif (index2 == 2):
-        print(character + " found at index 0")
-        print(character + " found at index 2")
-    elif (index2 == 3):
-        print(character + " found at index 0")
-        print(character + " found at index 3")
-    elif (index2 == 4):
+instances = 0
+if (character == word[0]):
+    if (character == word[1]):
+        if (character == word[2]):
+            if (character == word[3]):
+                if (character == word[4]):
+                    print(character + " found at index 0")
+                    print(character + " found at index 1")
+                    print(character + " found at index 2")
+                    print(character + " found at index 3")
+                    print(character + " found at index 4")
+                    instances = instances + 5
+                else: 
+                    print(character + " found at index 0")
+                    print(character + " found at index 1")
+                    print(character + " found at index 2")
+                    print(character + " found at index 3")
+                    instances = instances + 4
+            elif(character == word[4]):
+                print(character + " found at index 0")
+                print(character + " found at index 1")
+                print(character + " found at index 2")
+                print(character + " found at index 4")
+                instances = instances + 4
+        elif (character == word[3]):
+            if (character == word[4]):
+                print(character + " found at index 0")
+                print(character + " found at index 1")
+                print(character + " found at index 3")
+                print(character + " found at index 4")
+                instances = instances + 4
+            else: 
+                print(character + " found at index 0")
+                print(character + " found at index 1")
+                print(character + " found at index 3")
+                instances = instances + 3
+        elif (character == word[4]):
+            print(character + " found at index 0")
+            print(character + " found at index 1")
+            print(character + " found at index 4")
+            instances = instances + 3
+        else: 
+            print(character + " found at index 0")
+            print(character + " found at index 1")
+            instances = instances + 2
+    elif (character == word[2]):
+        if (character == word[3]):
+            if (character == word[4]):
+                print(character + " found at index 0")
+                print(character + " found at index 2")
+                print(character + " found at index 3")
+                print(character + " found at index 4")
+                instances = instances + 4
+            else: 
+                print(character + " found at index 0")
+                print(character + " found at index 2")
+                print(character + " found at index 3")
+                instances = instances + 3
+        elif (character == word[4]):
+            print(character + " found at index 0")
+            print(character + " found at index 2")
+            print(character + " found at index 4")
+            instances = instances + 3
+        else: 
+            print(character + " found at index 0")
+            print(character + " found at index 2")
+            instances = instances + 2
+    elif (character == word[3]):
+        if (character == word[4]):
+            print(character + " found at index 0")
+            print(character + " found at index 3")
+            print(character + " found at index 4")
+            instances = instances + 3
+        else: 
+            print(character + " found at index 0")
+            print(character + " found at index 3")
+            instances = instances + 2
+    elif (character == word[4]):
         print(character + " found at index 0")
         print(character + " found at index 4")
+        instances = instances + 2
     else: print(character + " found at index 0")
-elif (index == 1):
-    if (index2 == 2):
-        print(character + " found at index 1")
-        print(character + " found at index 2")
-    elif (index2 == 3):
-        print(character + " found at index 1")
-        print(character + " found at index 3")
-    elif (index2 == 4):
+    instances = instances + 1
+elif (character == word[1]):
+    if (character == word[2]):
+        if (character == word[3]):
+            if (character == word[4]):
+                    print(character + " found at index 1")
+                    print(character + " found at index 2")
+                    print(character + " found at index 3")
+                    print(character + " found at index 4")
+                    instances = instances + 4
+            else: 
+                    print(character + " found at index 1")
+                    print(character + " found at index 2")
+                    print(character + " found at index 3")
+                    instances = instances + 3
+        elif(character == word[4]):
+                print(character + " found at index 1")
+                print(character + " found at index 2")
+                print(character + " found at index 4")
+                instances = instances + 3
+        else:
+            print(character + " found at index 1")
+            print(character + " found at index 2")
+            instances = instances + 2
+    elif (character == word[3]):
+        if (character == word[4]):
+            print(character + " found at index 1")
+            print(character + " found at index 3")
+            print(character + " found at index 4")
+            instances = instances + 3
+        else: 
+            print(character + " found at index 1")
+            print(character + " found at index 3")
+            instances = instances + 2
+    elif (character == word[4]):
         print(character + " found at index 1")
         print(character + " found at index 4")
-    else: print(character + " found at index 1")
-elif (index == 2):
-    if (index2 == 3):
-        print(character + " found at index 2")
-        print(character + " found at index 3")
-    elif (index2 == 4):
+        instances = instances + 2
+    else: 
+        print(character + " found at index 1")
+        instances = instances + 1
+elif (character == word[2]):
+    if (character == word[3]):
+        if (character == word[4]):
+                print(character + " found at index 2")
+                print(character + " found at index 3")
+                print(character + " found at index 4")
+                instances = instances + 3
+        else: 
+            print(character + " found at index 2")
+            print(character + " found at index 3")
+            instances = instances + 2
+    elif(character == word[4]):
         print(character + " found at index 2")
         print(character + " found at index 4")
-    else: print(character + " found at index 2")
-elif (index == 3):
-    if (index2 == 4):
-        print(character + " found at index 3")
+        instances = instances + 2
+    else:
+        print(character + " found at index 2")
+        instances = instances + 1
+elif (character == word[3]):
+        if (character == word[4]):
+            print(character + " found at index 3")
+            print(character + " found at index 4")
+            instances = instances + 2
+        else: 
+            print(character + " found at index 3")
+            instances = instances + 1
+elif (character == word[4]):
         print(character + " found at index 4")
-    else: print(character + " found at index 3")
-elif (index == 4):
-    print(character + " found at index 4")
-if (index == -1):
-    instances = 0
-elif (index > -1):
-    if (index2 > index):
-        instances = 2
-    else: instances = 1
-if (instances == 1):
-    print("1 instance of " + character + " found in " + word)
+        instances = instances + 1
+if (instances == 5):
+    print("5 instances of " + character + " found in " + word)
+elif (instances == 4):
+    print("4 instances of " + character + " found in " + word)
+elif (instances == 3):
+    print("3 instances of " + character + " found in " + word)
 elif (instances == 2):
     print("2 instances of " + character + " found in " + word)
+elif (instances == 1):
+    print("1 instance of " + character + " found in " + word)
 else: print("No instances of " + character + " found in " + word)
