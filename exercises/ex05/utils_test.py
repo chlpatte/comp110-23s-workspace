f9d9a861ee280ec0a1c808a318b547564b6f98ac
Christarian Woods Patterson <chlpatte@unc.edu>
2023-03-08 20:29:52 -0500

    Progress on ex05

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -14,7 +14,9 @@ def test_evens() -> None:
     assert only_evens(test_list) == [2, 6, 8]
 
 
-#def test_() -> None:
+def test_negatives() -> None:
+    test_list: list[int] = [-2, -5, -7, -8]
+    assert only_evens(test_list) == [-2, -8]
 
 
 def test_order() -> None:
@@ -29,7 +31,10 @@ def test_repetition() -> None:
     assert concat(test_list1, test_list2) == [1, 2, 2, 2, 2, 2]
 
 
-#def test_
+def test_non_matching_len() -> None:
+    test_list1: list[int] = [1, 2]
+    test_list2: list[int] = [3, 4, 5, 6, 7]
+    assert concat(test_list1, test_list2) == [1, 2, 3, 4, 5, 6, 7]
 
 
 def test_range_of_none() -> None:
@@ -42,4 +47,6 @@ def test_range_out_of_index() -> None:
     assert sub(test_list, -1, 4) == [0, 1, 2]
 
 
-#def test_
+def test_range_of_one() -> None:
+    test_list: list[int] = [0, 1, 2]
+    assert sub(test_list, 0, 1) == [0]
