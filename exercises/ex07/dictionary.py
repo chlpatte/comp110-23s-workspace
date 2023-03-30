65a7206df6b0af6c4fd64df0a8e693f580f17c54
Christarian Woods Patterson <chlpatte@unc.edu>
2023-03-30 15:49:31 -0400

    progress on ex07

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -4,7 +4,47 @@ __author__ = "730580489"
 
 
 def invert(inp_dict: dict[str, str]) -> dict[str, str]:
-    out_dict: dict[str, str] = dict()
+    """Will make the keys the value and the value the keys."""
+    tracking_list: list[str] = []
+    for i in inp_dict:
+        tracking_list.append(inp_dict[i])
+    num: int = 1
+    for i in tracking_list:
+        if tracking_list.count(i) > num:
+            raise KeyError("Keys in a dictionary are unique. This argument would return two of the same value.")
+    out_dict: dict[str, str] = {}
     for i in inp_dict:
         out_dict[inp_dict[i]] = i
-    raise KeyError("Keys in a dictionary must be unique, you cannot have two of the same.")
+    return out_dict
+
+
+def favorite_color(inp_dict: dict[str, str]) -> str:
+    """Will count the most repeated value in a given dictionary and return it."""
+    tracking_list: list[str] = []
+    for i in inp_dict:
+        tracking_list.append(inp_dict[i])
+    idx: int = 0
+    num: int = 0
+    cur_max: int = 1
+    for i in tracking_list:
+        for x in tracking_list:
+            if i == x:
+                num += 1
+        if num > cur_max:
+            cur_max = num
+            num = 0
+            idx = tracking_list.index(i)
+        else:
+            num = 0
+    return tracking_list[idx]
+
+
+def count(inp_list: list[str]) -> dict[str, int]:
+    """Counts the amount of times an element appears in a list and creates a dictionary with the element as the key and its quanity as its value."""
+    out_dict: dict[str, int] = {}
+    for i in inp_list:
+        if i in out_dict:
+            out_dict[i] = out_dict[i] + 1
+        else:
+            out_dict[i] = 1
+    return out_dict
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -2,10 +2,58 @@
 
 __author__ = "730580489"
 
-import pytest
+from exercises.ex07.dictionary import invert, favorite_color, count
 
-from exercises.ex07.dictionary import invert
 
-with pytest.raises(KeyError):
-    my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
-    invert(my_dictionary)
+def test_empty_dict() -> None:
+    """Make sures when given an empty dictionary it returns an empty dictionary."""
+    test_dict: dict[str, str] = {}
+    assert invert(test_dict) == {}
+
+
+def test_value_equals_key() -> None:
+    """Makes sure the value can be the key as well."""
+    test_dict: dict[str, str] = {"jordan": "jordan", "cole": "cole"}
+    assert invert(test_dict) == {"jordan": "jordan", "cole": "cole"}
+
+
+def test_length() -> None:
+    """Makes sure the key and value are inverted no matter the length or lack there of."""
+    test_dict: dict[str, str] = {"This should work": "", "": "I pray this works, I really need it to"}
+    assert invert(test_dict) == {'': 'This should work', 'I pray this works, I really need it to': ''}
+
+
+def test_only_one_color() -> None:
+    """Makes sure the color is returned even if its the only color"""
+    test_dict: dict[str, str] = {"k": "blue", "l": "blue", "m": "blue"}
+    assert favorite_color(test_dict) == "blue"
+
+
+def test_earliest_color() -> None:
+    """Makes sure the earliest color is returned if no other color is repeated more."""
+    test_dict: dict[str, str] = {"1": "blue", "2": "blue", "3": "red", "4": "red"}
+    assert favorite_color(test_dict) == "blue"
+
+
+def test_only_values() -> None:
+    """Makes sure only values are being counted towards the total."""
+    test_dict: dict[str, str] = {"blue": "blue", "blue": "red", "blue": "red"}
+    assert favorite_color(test_dict) == "red"
+
+
+def test_empty_list() -> None:
+    """Makes sure an empty list returns an empty dictionary."""
+    test_list: list[str] = []
+    assert count(test_list) == {}
+
+
+def test_counts_whitespace_and_nospace() -> None:
+    """Makes sure whitespaces and nospaces inside of quotation marks are counted and returned in a dictionary."""
+    test_list: list[str] = ["", "", "", " ", " ", " ", " ", " ", " "]
+    assert count(test_list) == {"": 3, " ": 6}
+
+
+def test_capitalization() -> None:
+    """Makes sure capitalization at any point in a string makes a difference."""
+    test_list: list[str] = ["lee", "Lee", "lEe", "leE", "LeE", "lEE", "LEE", "lee", "lee", "LEE"]
+    assert count(test_list) == {"lee": 3, "Lee": 1, "lEe": 1, "leE": 1, "LeE": 1, "lEE": 1, "LEE": 2}
